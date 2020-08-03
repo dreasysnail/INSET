f74ce4fa478e4684328502314590f5504c544ee8
Yichen Huang <yichuang@mit.edu>
2020-08-03 19:13:18 -0400

    Update INSET_test.py

--- INSET_test.py
+++ INSET_test.py
@@ -11,18 +11,14 @@ import tqdm
 
 import sys
 import torch
-from pytorch_pretrained_bert_inset import GPT2Tokenizer, GPT2LMHeadModel, GPT2Config, BertTokenizer, BertModel, BertModelSent, BertModelWord
+from pytorch_pretrained_bert_inset import GPT2Tokenizer, GPT2LMHeadModel, GPT2Config, BertTokenizer, BertModel, BertModelSent
 from gpt2_training.train_utils_auto import load_model
 from gpt2_training.generation_auto import generate_sequence, beam_search_naive
 
-# from nltk.corpus import stopwords
 from torch.nn.utils.rnn import pad_sequence
 
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
-
-from queue import Queue
-from threading import Thread
 
 class ARGS:
     def __init__(
@@ -52,25 +48,21 @@ def prepare_for_bleu(sentence):
 if __name__ == "__main__":
     args = ARGS()
     print('decode type: ', args.type)
-    enc = GPT2Tokenizer.from_pretrained('/azure/yichen/model/117M')
+    enc = GPT2Tokenizer.from_pretrained('models/117M')
     tokenizer_bert = BertTokenizer.from_pretrained('bert-base-uncased')
 
-    # model_bert = BertModel.from_pretrained('bert-base-uncased', state_dict=torch.load('/azure/yichen/tgbert/gpt/cr/GPT2.2e-05.80.1gpu.2020-05-01153259/BERT-pretrain-1-step-40000.pkl')).cuda()
-    model_bert = BertModel.from_pretrained('bert-base-uncased', state_dict=torch.load('/azure/yichen/outputs/GPT2.0.0001.64.1gpu.2019-08-26051006/BERT-pretrain-1-step-5000.pkl')).cuda()
+    model_bert = BertModel.from_pretrained('bert-base-uncased', state_dict=torch.load('models/BERT-pretrain-1-step-5000.pkl')).cuda()
     model_bert.eval()
-    config = GPT2Config.from_json_file('/azure/yichen/model/117M/config.json')
-    # model_gpt = load_model(GPT2LMHeadModel(config), '/azure/yichen/tgbert/gpt/cr/GPT2.2e-05.80.1gpu.2020-05-01153259/GPT2-pretrain-1-step-40000.pkl', args).cuda()
-    # model_pre = load_model(GPT2LMHeadModel(config), '/azure/yichen/tgbert/gpt/cr/GPT2.2e-05.80.1gpu.2020-05-01153259/PRE-pretrain-1-step-40000.pkl', args).cuda()
-    model_gpt = load_model(GPT2LMHeadModel(config), '/azure/yichen/outputs/GPT2.0.0001.64.1gpu.2019-08-26051006/GPT2-pretrain-1-step-5000.pkl', args).cuda()
-    model_pre = load_model(GPT2LMHeadModel(config), '/azure/yichen/outputs/GPT2.0.0001.64.1gpu.2019-08-26051006/PRE-pretrain-1-step-5000.pkl', args).cuda()
+    config = GPT2Config.from_json_file('models/117M/config.json')
+    model_gpt = load_model(GPT2LMHeadModel(config), 'models/GPT2-pretrain-1-step-5000.pkl', args).cuda()
+    model_pre = load_model(GPT2LMHeadModel(config), 'models/PRE-pretrain-1-step-5000.pkl', args).cuda()
     model_pre.eval()
     model_gpt.eval()
-    w = torch.load('/azure/yichen/tgbert/reviews/trip/weight.pt')
-    b = torch.load('/azure/yichen/tgbert/reviews/trip/bias.pt')
+    w = torch.load('models/weight.pt')
+    b = torch.load('models/bias.pt')
 
     # no lexical hints
-    # bert_sent_no_key = BertModelSent.from_pretrained('bert-base-uncased', state_dict=torch.load('/azure/yichen/outputs/GPT2.0.001.1024.2gpu.2020-05-02040426/BERTsent-6-step-1072.pkl')).cuda()
-    bert_sent_no_key = BertModelSent.from_pretrained('bert-base-uncased', state_dict=torch.load('/azure/yichen/outputs/GPT2.0.0001.640.1gpu.2019-09-22180055/BERTsent-8-step-1721.pkl')).cuda()
+    bert_sent_no_key = BertModelSent.from_pretrained('bert-base-uncased', state_dict=torch.load('models/BERTsent-8-step-1721.pkl')).cuda()
     bert_sent_no_key.eval()
 
     with open('input.txt') as input_file:
@@ -104,10 +96,8 @@ if __name__ == "__main__":
                 prediction = prediction * w + b
 
                 context = model_pre(input_ids=None, position_ids=None, token_type_ids=None, lm_labels=None, past=None, history=prediction.unsqueeze(0))
-                # out = generate_sequence(model_gpt, 1, length=args.max_seq_length, temperature=args.temperature, top_k=args.top_k, sample=args.is_sampling, past=context)
             
                 if args.type == 'greedy':
-                    # out = beam_search_naive(model_gpt, 1, length=args.max_seq_length, beam_width=1, beam_examples=1, past=context)
                     out = generate_sequence(model_gpt, 1, length=args.max_seq_length, temperature=1, top_k=1, sample=False, past=context)
                     out = out.tolist()
                     gen = [enc.decode(prepare_for_bleu(s)) for s in out]
